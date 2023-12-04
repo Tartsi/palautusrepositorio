8457a40fbfa65a56f1d8ecda31989f3e3c124497
Tartsi <tarvo.lilja@hotmail.fi>
2023-12-04 19:24:00 +0200

    4. Laskin ja komento-oliot

--- viikko5/laskin/src/kayttoliittyma.py
+++ viikko5/laskin/src/kayttoliittyma.py
@@ -15,6 +15,40 @@ class Kayttoliittyma:
         self._root = root
 
     def kaynnista(self):
+        self._alusta_UI()
+
+    def _hae_syote(self):
+        try:
+            return int(self._syote_kentta.get())
+        except Exception:
+            pass
+
+    def _komento_toiminta(self, komento, arvo):
+
+        if komento == Komento.SUMMA:
+            self._sovelluslogiikka.plus(arvo)
+        elif komento == Komento.EROTUS:
+            self._sovelluslogiikka.miinus(arvo)
+        elif komento == Komento.NOLLAUS:
+            self._sovelluslogiikka.nollaa()
+        elif komento == Komento.KUMOA:
+            pass
+
+    def _suorita_komento(self, komento):
+
+        self._komento_toiminta(komento, self._hae_syote())
+
+        self._kumoa_painike["state"] = constants.NORMAL
+
+        if self._sovelluslogiikka.arvo() == 0:
+            self._nollaus_painike["state"] = constants.DISABLED
+        else:
+            self._nollaus_painike["state"] = constants.NORMAL
+
+        self._syote_kentta.delete(0, constants.END)
+        self._arvo_var.set(self._sovelluslogiikka.arvo())
+
+    def _alusta_UI(self):
         self._arvo_var = StringVar()
         self._arvo_var.set(self._sovelluslogiikka.arvo())
         self._syote_kentta = ttk.Entry(master=self._root)
@@ -48,35 +82,9 @@ class Kayttoliittyma:
         )
 
         tulos_teksti.grid(columnspan=4)
-        self._syote_kentta.grid(columnspan=4, sticky=(constants.E, constants.W))
+        self._syote_kentta.grid(
+            columnspan=4, sticky=(constants.E, constants.W))
         summa_painike.grid(row=2, column=0)
         erotus_painike.grid(row=2, column=1)
         self._nollaus_painike.grid(row=2, column=2)
         self._kumoa_painike.grid(row=2, column=3)
-
-    def _suorita_komento(self, komento):
-        arvo = 0
-
-        try:
-            arvo = int(self._syote_kentta.get())
-        except Exception:
-            pass
-
-        if komento == Komento.SUMMA:
-            self._sovelluslogiikka.plus(arvo)
-        elif komento == Komento.EROTUS:
-            self._sovelluslogiikka.miinus(arvo)
-        elif komento == Komento.NOLLAUS:
-            self._sovelluslogiikka.nollaa()
-        elif komento == Komento.KUMOA:
-            pass
-
-        self._kumoa_painike["state"] = constants.NORMAL
-
-        if self._sovelluslogiikka.arvo() == 0:
-            self._nollaus_painike["state"] = constants.DISABLED
-        else:
-            self._nollaus_painike["state"] = constants.NORMAL
-
-        self._syote_kentta.delete(0, constants.END)
-        self._arvo_var.set(self._sovelluslogiikka.arvo())
